fee4269c5a1c47af95d5bf0e8ce6dec154c2ef13
divyansh-kumar <kumardivyansh59@gmail.com>
2023-11-22 21:32:43 -0500

    message successfully displayed

---
+++ Assignment6/Part1/main.js
@@ -0,0 +1,13 @@
+const storyText = 'test :insertx: one :inserty: oooo :insertz:';
+const insertX = ['a', 'b', 'c'];
+const insertY = ['d', 'e', 'f'];
+const insertZ = ['g', 'h', 'i'];
+
+const button = document.querySelector('.randomize');
+const paragraph = document.querySelector('.story');
+
+
+
+
+paragraph.textContent = "hgfvhjvjhjnbkjbjb";
+paragraph.style.visibility = 'visible';
