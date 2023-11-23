add25ce6d9d5d002555b3bb56f6405e3ecc2c15d
divyansh-kumar <kumardivyansh59@gmail.com>
2023-11-22 22:12:38 -0500

    images showing

--- Assignment6/Part2/main.js
+++ Assignment6/Part2/main.js
@@ -10,14 +10,16 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
+const images = ["pic1.jpg","pic2.jpg","pic3.jpg","pic4.jpg","pic5.jpg"];
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for (let i = 1; i <= images.length; i++) {
+	const newImage = document.createElement('img');
+	newImage.setAttribute('src', `images/pic${i}.jpg`);
+	thumbBar.appendChild(newImage);
+	
+  }
 
 /* Wiring up the Darken/Lighten button */
