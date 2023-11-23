f7479d3737e45dcb49d0a256aae5b50950d65ee3
divyansh-kumar <kumardivyansh59@gmail.com>
2023-11-22 22:37:21 -0500

    implemented the code from the website

--- Assignment6/Part4/main-finished.js
+++ Assignment6/Part4/main-finished.js
@@ -64,19 +64,38 @@ class Ball {
 
    collisionDetect() {
       for (const ball of balls) {
-         if (!(this === ball)) {
-            const dx = this.x - ball.x;
-            const dy = this.y - ball.y;
-            const distance = Math.sqrt(dx * dx + dy * dy);
-
-            if (distance < this.size + ball.size) {
-              ball.color = this.color = randomRGB();
-            }
-         }
+        if (!(this === ball) && ball.exists) {
+          const dx = this.x - ball.x;
+          const dy = this.y - ball.y;
+          const distance = Math.sqrt(dx * dx + dy * dy);
+    
+          if (distance < this.size + ball.size) {
+            ball.color = this.color = randomRGB();
+          }
+        }
       }
-   }
+    }
+    
 
 }
+
+window.addEventListener("keydown", (e) => {
+   switch (e.key) {
+     case "a":
+       this.x -= this.velX;
+       break;
+     case "d":
+       this.x += this.velX;
+       break;
+     case "w":
+       this.y -= this.velY;
+       break;
+     case "s":
+       this.y += this.velY;
+       break;
+   }
+ });
+ 
 
 const balls = [];
 
